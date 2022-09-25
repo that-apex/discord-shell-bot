f597c36fc648a0dd27114a6fe7408a4a8622d695
that-apex <contact@thatapex.dev>
2022-09-26 00:54:28 +0200

    add stderr

--- src/main/kotlin/net/thatapex/shellbot/ssh/jsch/JSchShell.kt
+++ src/main/kotlin/net/thatapex/shellbot/ssh/jsch/JSchShell.kt
@@ -4,26 +4,38 @@ import com.jcraft.jsch.ChannelShell
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import net.thatapex.shellbot.ssh.SSHShell
+import java.io.InputStream
 import java.io.PrintStream
 import kotlin.math.min
 
 class JSchShell(private val channel: ChannelShell) : SSHShell {
 
-    private val output = PrintStream(channel.outputStream)
-    private val input = channel.inputStream
+    private val stdin = PrintStream(channel.outputStream)
+    private val stdout = channel.inputStream
+    private val stderr = channel.extInputStream
 
     override val open: Boolean
         get() = this.channel.isConnected
 
     override suspend fun readData(): String {
+        val stderr = read(stderr)
+
+        if (stderr.isNotEmpty()) {
+            return stderr
+        }
+
+        return read(stdout)
+    }
+
+    private suspend fun read(stream: InputStream): String {
         return withContext(Dispatchers.IO) {
-            val available = input.available()
+            val available = stream.available()
 
             if (available < 0) {
-                return@withContext ""
+                ""
             } else {
                 val data = ByteArray(min(MAX_BUFFER_SIZE, available))
-                input.read(data)
+                stream.read(data)
                 data.decodeToString()
             }
         }
@@ -31,20 +43,22 @@ class JSchShell(private val channel: ChannelShell) : SSHShell {
 
     override suspend fun sendCommand(command: String) {
         withContext(Dispatchers.IO) {
-            output.print(command)
-            output.println("\n")
-            output.flush()
+            stdin.print(command)
+            stdin.print("\n")
+            stdin.flush()
         }
     }
 
     override fun close() {
-        output.close()
+        stdin.close()
+        stdin.close()
+        stderr.close()
 
         if (!channel.isClosed)
             channel.disconnect()
     }
 
     companion object {
-        private const val MAX_BUFFER_SIZE = 1024
+        private const val MAX_BUFFER_SIZE = 2048
     }
 }
